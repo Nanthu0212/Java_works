b098430e98117e5bf89173bfa301d4624f8745b5
Nantha kishore N <nanthakishorenatarajan2005@gmail.com>
2026-06-06 08:11:39 +0530

    Add files via upload

---
+++ Task31.java
@@ -0,0 +1,14 @@
+public class Task31 {
+    public static void main(String[] args){
+        for (int i = 1; i <= 5; i++){
+            for (int j = 1; j <=5 ;j++){
+                if (i == 1 || i == 5 || j == 1 || j == 5){
+                    System.out.print( " 1 ");
+                } else {
+                    System.out.print("   ");
+                }
+            }
+            System.out.println(); // Move to the next line after each row
+        }
+    }
+}
