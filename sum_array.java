cb7dffc0bdfa3643fc613a51fb6cac8b8ade512a
Nantha kishore N <nanthakishorenatarajan2005@gmail.com>
2026-07-19 12:11:05 +0530

    Add files via upload

---
+++ sum_array.java
@@ -0,0 +1,12 @@
+public class sum_array {
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,27,28,29,20};
+        int sum = 0;
+        for(int i =0; i<arr.length; i++){
+            sum += arr[i];
+        }
+        System.out.println("The sum of the array is : " + sum);
+
+    }
+    
+}
